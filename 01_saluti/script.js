cf4584c3ada9ba359d07ec1ebdcda621ac2eb608
assettasimone <assettasimone@gmail.com>
2025-07-19 09:16:19 +0200

    first commit

---
+++ 01_saluti/script.js
@@ -0,0 +1,13 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+
+// Dichiara la funzione qui.
+const salutoVeloce = (nomeUtente) => 'Ciao ' + nomeUtente;
+
+// Invoca la funzione qui e stampa il risultato in console
+
+console.log(salutoVeloce(userName))
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
