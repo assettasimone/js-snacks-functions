d42b939128e007ce8c47beef1d78e65c3201d6d4
assettasimone <assettasimone@gmail.com>
2025-07-19 09:40:09 +0200

    add 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,23 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const userLetter = prompt('inserisci una lettere');
 
 // Dichiara la funzione qui.
+const checkFirstLetter = (names, letter) => {
+    let paroleTrovate = [];
+    for (let i = 0; i < names.length; i++) {
+        const firstLetter = names[i].charAt(0);
+        if (firstLetter === letter) {
+            paroleTrovate.push(names[i]);
+        }
 
+    }
+    return paroleTrovate
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(checkFirstLetter(names, userLetter));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
