38210a217cba06274c1ea924236149e7bd8d705d
assettasimone <assettasimone@gmail.com>
2025-07-19 09:30:24 +0200

    add 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function inizialiNome(names) {
+    let iniziali = [];
 
+    for (let i = 0; i < names.length; i++) {
+        const singleName = names[i];
+        iniziali[i] = singleName.charAt(0)
+    }
+
+    return iniziali
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(inizialiNome(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
